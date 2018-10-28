517410c3f5acb3161928710a8f47cf17a8284d8a
Alexander George Legaspi <lex83nyc@gmail.com>
2018-10-28 14:24:28 -0400

    updated search component now dynamically stores and fetches

--- app/javascript/components/Search.jsx
+++ app/javascript/components/Search.jsx
@@ -31,3 +31,15 @@ export default class Search extends React.Component {
     );
   }
 }
+
+
+
+/* 
+
+- To start, we defined our components state (and initial state) to include our search results and whether or not we’re currently loading/fetching new results.
+
+- Next, we wrote our onChange function which gets called each time the value in the search field changes. We use axios to send an http request to our new /search.json endpoint with the current search field text. Axios will either successfully fetch results in which case we update our state to include the results, or it will fail and we update our state to have no results.
+
+- Our render function stays almost the same. We alter the input field by adding an onChange handler and pointing to the onChange function we just wrote.
+
+*/
